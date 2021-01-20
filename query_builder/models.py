a015a23bdc6f47f7a545cc6ec0c7f5de274aad4e
Roy Tancredi <tancredi1994@gmail.com>
2021-01-20 13:51:57 +0100

    Added some basics of models

--- query_builder/models.py
+++ query_builder/models.py
@@ -1,6 +1,38 @@
 from django.db import models
 
 
+class SearchOption(models.Model):
+    """
+    Represents search operators/special operators and more advenced search option, 
+    like changing location or language, etc.
+
+    ...
+    Atributes
+    ---------
+    descritor : <str>
+        Distinctive and diserable name of given option.
+
+    ascriptors : <str>
+        Synonims and alternatives for descriptor.
+
+    description : <str>
+        Description of option usage.
+
+    # SOON....
+    """
+    descriptor = models.CharField(max_length=64)
+    # TODO \/ Need to be converted to some iterable.
+    ascriptors = models.CharField(max_length=256)
+    description = models.TextField()
+    tips = models.CharField(max_length=256)
+
+    class Meta:
+        ordering = ['descriptor']
+
+    def __str__(self):
+        return f'{self.descriptor} Option'
+
+
 class SearchEngine(models.Model):
     """
     Represents a search engine.
@@ -17,22 +49,35 @@ class SearchEngine(models.Model):
     icon_path : <str>
         The path to search engine brand icon.
 
+    option : <ManyToManyField>
+        Relation to Option Model through Invocation Model.
+
     """
 
-    name = models.CharField(max_length=30)
-    get_url = models.CharField(max_length=50)
-    icon_path = models.CharField(max_length=100)
+    name = models.CharField(max_length=32)
+    get_url = models.CharField(max_length=64)
+    icon_path = models.CharField(max_length=128)
+    # ! Need to add related_name if there are another field in other model with same name?
+    option = models.ManyToManyField(SearchOption, through='Invocation')
 
     def __str__(self):
-        return self.name
+        return f'{self.name} Engine'
 
 
-class SearchOption(models.Model):
-    descriptor = models.CharField(max_length=50)
-    ascriptors = models.CharField(max_length=200)
-    engine = models.ManyToManyField(SearchEngine)
+class Invocation(models.Model):
+    """
+    Contains specific usage parametrs for specific option in various search engines.
+
+    ...
+    Atributes
+    ---------
+    # SOON...
+
+    """
+    engine = models.ForeignKey(SearchEngine, on_delete=models.CASCADE)
+    option = models.ForeignKey(SearchOption, on_delete=models.CASCADE)
+    parameter_construct = models.CharField(max_length=128)
+    extra_tips = models.CharField(max_length=256)
 
     def __str__(self):
-        return self.descriptor
-
-# class SearchOptionValue(models.Model):
+        return f'Invocation of {self.option} for {self.engine}'
