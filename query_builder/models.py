35d3ae19d81b0d05cdfaa56fcbcc36b986972aef
Roy Tancredi <tancredi1994@gmail.com>
2021-01-29 07:26:22 +0100

    Model's docstrings added

--- query_builder/models.py
+++ query_builder/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
 
-class SearchOption(models.Model):
+class SettingValue(models.Model):
+    """
+    SOON...
+    """
+    value = models.CharField(max_length=128)
+    human_readable = models.CharField(max_length=128)
+
+    def __str__(self):
+        return 'Value'
+
+
+class SearchSetting(models.Model):
     """
     Represents search operators/special operators and more advenced search option, 
     like changing location or language, etc.
@@ -18,13 +29,20 @@ class SearchOption(models.Model):
     description : <str>
         Description of option usage.
 
-    # SOON....
+    values : <ManyToManyField>
+        Setting's values if any specific.
+
+    tips : <str>
+        Independent of engine general clues for using specific setting.
     """
-    descriptor = models.CharField(max_length=64)
-    # TODO \/ Need to be converted to some iterable.
-    ascriptors = models.CharField(max_length=256)
+    descriptor = models.CharField(
+        'the most adequate setting\'s name', max_length=64, unique=True)
+    ascriptors = models.CharField(
+        'verbal equivalents to setting\'s name', max_length=256, blank=True)
     description = models.TextField()
-    tips = models.CharField(max_length=256)
+    values = models.ManyToManyField(SettingValue)
+    tips = models.CharField(
+        'general clues for using specific setting', max_length=256, blank=True)
 
     class Meta:
         ordering = ['descriptor']
@@ -49,16 +67,18 @@ class SearchEngine(models.Model):
     icon_path : <str>
         The path to search engine brand icon.
 
-    option : <ManyToManyField>
-        Relation to Option Model through Invocation Model.
+    setting : <ManyToManyField>
+        Relation to SearchSetting Model through Invocation Model.
 
     """
 
-    name = models.CharField(max_length=32)
-    get_url = models.CharField(max_length=64)
-    icon_path = models.CharField(max_length=128)
-    # ! Need to add related_name if there are another field in other model with same name?
-    option = models.ManyToManyField(SearchOption, through='Invocation')
+    name = models.CharField(max_length=32, unique=True)
+    get_url = models.CharField(max_length=64, unique=True)
+    icon_path = models.CharField(max_length=128, blank=True)
+    settings = models.ManyToManyField(SearchSetting, through='Invocation')
+
+    class Meta:
+        ordering = ['name']
 
     def __str__(self):
         return f'{self.name} Engine'
@@ -71,13 +91,26 @@ class Invocation(models.Model):
     ...
     Atributes
     ---------
-    # SOON...
+    engine : <ForeignKey>
+        Relation to SearchEngine Model.
+
+    setting : <ForeignKey>
+        Relation to SearchSetting Model.
+
+    param_construct : <str>
+        Specific parameter construction - <keyword>=<placeholder> in general.
+
+    extra_tips : <str>
+        Tips for usage setting in specific search engine.
 
     """
+
     engine = models.ForeignKey(SearchEngine, on_delete=models.CASCADE)
-    option = models.ForeignKey(SearchOption, on_delete=models.CASCADE)
-    parameter_construct = models.CharField(max_length=128)
-    extra_tips = models.CharField(max_length=256)
+    setting = models.ForeignKey(SearchSetting, on_delete=models.CASCADE)
+    param_construct = models.CharField(
+        'search parameter key and syntax', max_length=128)
+    extra_tips = models.CharField(
+        'tips for usage in specific engine', max_length=256, blank=True)
 
     def __str__(self):
-        return f'Invocation of {self.option} for {self.engine}'
+        return f'Invocation of {self.setting} for {self.engine}'
